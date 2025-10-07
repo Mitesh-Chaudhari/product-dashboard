f9565c993f783f3bda78979cf054db61ff43da60
Mitesh Chaudhari <miteshchaudhari1999@gmail.com>
2025-10-07 20:59:08 +0530

    - deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 replace 'product-dashboard' with your repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/product-dashboard/', 
 })
