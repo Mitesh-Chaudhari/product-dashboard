4f594e22a07442639570e76611ed3f69ed917549
Mitesh Chaudhari <miteshchaudhari1999@gmail.com>
2025-10-07 21:23:04 +0530

    - removed github pages deployment settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace this with your repository name
-const repoName = 'product-dashboard';
-
-// If you deploy to a subpath (GitHub Pages)
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
 });
